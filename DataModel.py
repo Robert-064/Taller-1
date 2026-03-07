dc6315213195d55e536166b137f8a1a9494e078b
Robert Argaez <robertargaez1113652@correo.itm.edu.co>
2026-03-06 21:20:05 -0500

    Api Funcional

--- DataModel.py
+++ DataModel.py
@@ -1,1 +1,33 @@
-print("Esto es un ejemplo")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class reserva(BaseModel):
+    id_reserva : int
+    id_sala : int
+    id_usuario : int
+    fecha : str
+    hora_incio : str
+    hora_fin : str
+    personas: int
+    estado: str
+
+reservas = []
+
+@app.get("/")
+def inicio():
+    return{"mensaje":"API de reservas funcionado"}
+
+@app.post("/reservas")
+def crear_reservas(reserva:reserva):
+    reservas.append(reserva)
+    return {"mensaje":"Reserva registrada con exito"}
+
+@app.get("/reservas")
+def mostrar_reservas():
+        return reservas
+
+@app.get("/saludo")
+def saludo():
+    return {"mensaje": "Hola desde otra ruta"}
